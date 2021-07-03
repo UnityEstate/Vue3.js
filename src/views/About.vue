b2105e5ae703f84bee7531859ac91efd94428e0c
UnityEstate <unity.estate2019@gmail.com>
2021-07-03 17:15:31 +0700

    add setvue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>เกี่ยวกับเรา</h1>
   </div>
 </template>
+<style scoped>
+ .about {
+   color: red;
+ }
+
+</style>>
+</style>
+
