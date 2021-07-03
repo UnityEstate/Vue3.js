13d3854a38a04103d1ff02bf056d54c1186e6818
UnityEstate <unity.estate2019@gmail.com>
2021-07-03 19:17:03 +0700

    add hello world

---
+++ src/components/logo.vue
@@ -0,0 +1,15 @@
+<template>
+<img alt="Vue logo" src="../assets/logo.png" />
+  
+</template>
+
+<script>
+export default {
+    name : "Logo"
+
+};
+</script>
+
+<style>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="about">
     <h1>เกี่ยวกับเรา</h1>
+    <Logo />
   </div>
 </template>
+
+<script>
+  import Logo from "../components/Logo.vue";
+
+  export default {
+    name: "About",
+    components: {
+      Logo,
+    },
+  };
+</script>
+
 <style scoped>
  .about {
-   color: red;
- }
-
-</style>>
+   color: red; 
+   }
 </style>
 
