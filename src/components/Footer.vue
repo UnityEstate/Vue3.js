5770c1409e972b134d7b146621b22f20f5c3d160
UnityEstate <unity.estate2019@gmail.com>
2021-07-04 13:13:37 +0700

    add navbar

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid px-4">
+      <div class="d-flex align-items-center justify-content-between small">
+        <div class="text-muted">Copyright &copy; Your Website 2021</div>
+        <div>
+          <a href="#">Privacy Policy</a>
+          &middot;
+          <a href="#">Terms &amp; Conditions</a>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+</style>
